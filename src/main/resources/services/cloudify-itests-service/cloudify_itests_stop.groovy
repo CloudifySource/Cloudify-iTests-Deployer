f2154d887102a895b83e74e2be15910c9818b46f
Sagi Bernstein <sagioto@gmail.com>
2013-03-08 12:18:01 +0200

    Create cloudify_itests_stop.groovy

---
+++ src/main/resources/services/cloudify-itests-service/cloudify_itests_stop.groovy
@@ -0,0 +1,91 @@
+@Grapes(
+        @Grab(group='org.jclouds.api', module='s3', version='1.5.8')
+)
+import org.cloudifysource.dsl.context.ServiceContextFactory
+import org.cloudifysource.dsl.utils.ServiceUtils
+import org.jclouds.ContextBuilder
+import org.jclouds.blobstore.BlobStoreContext
+
+import java.util.logging.Logger    
+
+
+context = ServiceContextFactory.getServiceContext()
+Logger logger = Logger.getLogger(this.getClass().getName())
+
+
+
+//Only instance 1 does report and mergers
+if (context.instanceId == 1){
+  logger.info "service instance: 1 - merging and reporting"
+  
+  def executeMaven (mvnExec, String arguments, directory){
+      new AntBuilder().exec(executable: mvnExec,
+              failonerror:false,
+              dir:directory) {
+          env(key:'JAVA_HOME',value:"${System.getProperty("user.home")}/java")
+          arg(line: arguments)
+      }
+  }
+  
+
+  
+  serviceDir = "${System.getProperty("user.home")}/cloudify-itests-service"
+  config = new ConfigSlurper().parse(new File("cloudify-itests.properties").text)
+  
+  
+  def ext = ServiceUtils.isWindows() ? '.bat' : ''
+  def mvnBinDir = "${serviceDir}/maven/apache-maven-${config.maven.version}/bin"
+  def mvnExec = "${mvnBinDir}/mvn${ext}"
+  
+  strorageProps = new Properties()
+  strorageProps.load new FileInputStream(new File("${context.getServiceDirectory()}/credentials/cloud/ec2/ec2-cred.properties"))
+  storageConfig = new ConfigSlurper().parse(strorageProps)
+  provider = 's3'
+  blobStore  = ContextBuilder.newBuilder(provider)
+            .credentials("${storageConfig.user}", "${storageConfig.apiKey}")
+            .buildView(BlobStoreContext.class).getBlobStore()
+
+  containerName = "${config.test.TEST_RUN_ID}".toLowerCase()
+        
+
+  //Download from s3 bucket
+  logger.info "downloading the report files"
+  blobStore.list(containerName).eachParallel {
+    def outputFileName = "${serviceDir}/${config.test.SUITE_NAME}/${it.getName()}"
+    logger.info "downloding file ${it.getName()} to ${outputFileName}"
+    def input = blobStore.getBlob(containerName, it.getName()).getPayload().getInput()
+    def output = new FileOutputStream(new File(outputFileName))
+    read = 0
+    byte[] bytes = new byte[1024]
+
+    while ((read = input.read(bytes)) != -1) {
+      output.write(bytes, 0, read)
+    }
+
+    input.close()
+    output.flush()
+    output.close()
+  }
+  
+  logger.info "removing the container for the run"
+  blobStore.clearContainer(containerName)
+  blobStore.deleteContainer(containerName)
+
+  logger.info "running the tests reports merger"
+  versionSplit = "${config.cloudify.version}".split("\\.")
+  executeMaven(mvnExec,
+    "exec:java -Dexec.mainClass=\"framework.testng.report.TestsReportMerger\" -Dexec.args=\"${config.test.SUITE_NAME} ${config.test.BUILD_NUMBER}"
+                        + " ${serviceDir}/${config.test.SUITE_NAME} ${versionSplit[0]} ${versionSplit[1]}\" -Dcloudify.home=${buildDir}",
+                "${serviceDir}/${config.scm.projectName}")
+
+  logger.info "running the wiki reporter"
+  executeMaven(mvnExec,
+    "exec:java -Dexec.mainClass=\"framework.testng.report.wiki.WikiReporter\" -Dexec.args=\"${config.test.SUITE_TYPE} ${config.test.BUILD_NUMBER}"
+                        + " ${serviceDir}/${config.test.SUITE_NAME} ${versionSplit[0]} ${versionSplit[1]}\""
+                        + " -Dcloudify.home=${buildDir} -Dmysql.host=${config.test.MGT_MACHINE}",
+                "${serviceDir}/${config.scm.projectName}")
+
+}
+else{
+  logger.info "service instance: ${context.instanceId} - nothing to do on stop"
+}
