6f377a8bde56122c2169a487906d761407f1aa0f
sagioto <sagioto@gmail.com>
2013-04-04 17:30:04 +0300

    modified regex since it didn't work in script mode

--- src/main/java/itests_deployer.groovy
+++ src/main/java/itests_deployer.groovy
@@ -128,10 +128,10 @@ if (shouldBootstrap()){
 
     logger.info "inject mysql username and password"
     replaceTextInFile "${scriptDir}/../resources/services/mysql/mysql-service.properties",
-            ["^dbUser.*\$":"dbUser='${config.MYSQL_USER}'", "^dbPassW.*\$" : "dbPassW='${config.MYSQL_PASS}'"]
+            ['dbUser.*\n' : "dbUser='${config.MYSQL_USER}'\n", 'dbPassW.*\n' : "dbPassW='${config.MYSQL_PASS}'\n"]
 
     replaceTextInFile "${scriptDir}/../resources/services/tomcat/tomcat-service.properties",
-            ["^javaOpts.*\$":"javaOpts='-Dmysql.user=${config.MYSQL_USER} -Dmysql.pass=${config.MYSQL_PASS}'"]
+            ['javaOpts.*\n' : "javaOpts='-Dmysql.user=${config.MYSQL_USER} -Dmysql.pass=${config.MYSQL_PASS}'\n"]
 
 
 
