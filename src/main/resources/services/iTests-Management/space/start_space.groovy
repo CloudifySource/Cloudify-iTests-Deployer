4d31036e3755210f1cadd37738535dab89fbb8b5
sagioto <sagib@gigaspaces.com>
2013-04-11 13:47:39 +0300

    #4 now starting gsm for the space deployment

--- src/main/resources/services/iTests-Management/space/start_space.groovy
+++ src/main/resources/services/iTests-Management/space/start_space.groovy
@@ -1,18 +1,15 @@
 import org.cloudifysource.dsl.context.ServiceContextFactory
 import org.openspaces.admin.AdminFactory
+import org.openspaces.admin.gsa.GridServiceManagerOptions
 import org.openspaces.admin.pu.ProcessingUnitDeployment
 
 import java.util.concurrent.TimeUnit
 
 def context = ServiceContextFactory.getServiceContext()
-
-def factory = new AdminFactory();
-factory.addGroup(System.getProperty('com.gs.jini_lus.groups'))
-def admin = factory.createAdmin()
-
-
-
-def pu = admin.getGridServiceManagers().deploy(
+def admin = new AdminFactory().addGroup(System.getProperty('com.gs.jini_lus.groups')).createAdmin()
+def agent = admin.getGridServiceAgents().waitForAtLeastOne()
+def gsm = agent.startGridServiceAndWait(new GridServiceManagerOptions())
+def pu = gsm.deploy(
         new ProcessingUnitDeployment(
                 new File(
                         "${context.serviceDirectory}iTestsManagementSpace/target/iTestsManagementSpace.jar")))
